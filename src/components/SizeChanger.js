407af09aa01dfd61983d4ada28c496de6e5ad7e2
Josiah Maina <josiah5115@gmail.com>
2018-07-02 14:17:35 -0600

    inital commit

---
+++ src/components/SizeChanger.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+import React from 'react';
+
+export default function SizeChanger() {
+  return (
+    <select className="dropDownContainer">
+      <option value="12"> 12 </option>
+      <option value="13"> 13 </option>
+      <option value="14"> 14 </option>
+    </select>
+  )
+}
