f6e2b52b01c7ec48c5f364fb6f3c07ec54314574
Josiah Maina <josiah5115@gmail.com>
2018-07-02 18:33:14 -0600

    add defaulted

--- src/components/EditToggle.js
+++ src/components/EditToggle.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function EditToggle(props) {
   return (
-    <select className="dropDownContainer ml0" onChange={ (e) => props.update(e.target.event) }>
+    <select className="dropDownContainer ml0" onChange={ (e) => props.update(e.target.value) }>
       <option value="true"> Allow Edit </option>
       <option value="false"> Disable Edit </option>
     </select>
--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function SizeChanger() {
   return (
-    <select className="dropDownContainer" onChange={ (e) => props.update(e.target.event) }>
+    <select className="dropDownContainer" onChange={ (e) => props.update(parseInt(e.target.event) ) }  disable={ props.allowEdit === 'false' }>
       <option value="12"> 12 </option>
       <option value="13"> 13 </option>
       <option value="14"> 14 </option>
