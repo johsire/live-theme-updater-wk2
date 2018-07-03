6f5193f4018260aa3aff1015ff2c461da77872b3
Josiah Maina <josiah5115@gmail.com>
2018-07-02 19:26:05 -0600

    bind this/ bug fix

--- src/components/SizeChanger.js
+++ src/components/SizeChanger.js
@@ -2,12 +2,12 @@
 
 import React from 'react';
 
-export default function SizeChanger() {
+export default function SizeChanger(props) {
   return (
-    <select className="dropDownContainer" onChange={ (e) => props.update(parseInt(e.target.event) ) }  disable={ props.allowEdit === 'false' }>
+    <select className="dropDownContainer" onChange={ (e) => props.update( parseInt(e.target.value) ) } disabled={ props.allowEdit === "false" }>
       <option value="12"> 12 </option>
       <option value="13"> 13 </option>
       <option value="14"> 14 </option>
     </select>
   )
-}
+};
--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -4,23 +4,26 @@ import React, { Component } from 'react';
 
 export default class TextContainer extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       text: ''
-    }
+    };
+    this.updateText = this.updateText.bind(this);
   }
 
   updateText(e) {
     this.setState({
       text: e.target.value
-    })
+    });
   }
 
   render() {
     return (
       <div className="textContainer">
         <textarea 
-          // style={  }
+          style={ { fontFamily: this.props.fontFamily,
+                  fontSize: this.props.fontSize, 
+                  color: this.props.fontColor } }
           onChange={this.updateText}
           value={this.state.text} 
           placeholder='Start typing your thoughts here!'
